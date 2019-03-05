742021df283829eda8279e0d88a7babde8d9dd72
hidhayathunnaseera <43529922+hidhayathunnaseera@users.noreply.github.com>
2019-03-05 11:24:09 +0530

    Update pos.java

--- pos.java
+++ pos.java
@@ -17,7 +17,7 @@ class Java
         if(n>0)
             System.out.println("Positive");
         else if(n<0)
-            System.out.println(" Negative");
+            System.out.println("Negative");
         else
             System.out.println("Zero");
          
