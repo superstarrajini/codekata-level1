aa480b9b5488e2ef6ee12bf334b7e45f403ebcd2
hidhayathunnaseera <43529922+hidhayathunnaseera@users.noreply.github.com>
2019-03-05 11:20:10 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Java
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+        
+        Scanner sc=new Scanner(System.in);
+         
+        System.out.print("");
+        n=sc.nextInt();
+         
+        
+        if(n>0)
+            System.out.println("Positive");
+        else if(n<0)
+            System.out.println(" Negative");
+        else
+            System.out.println("Zero");
+         
+	}
+}
